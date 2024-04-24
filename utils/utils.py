bb50e969a9e2934c7289030b054a003b7b0f256d
cvilmorin <carre@aims.ac.za>
2024-04-24 11:50:57 +0200

    helps functions

---
+++ utils/utils.py
@@ -0,0 +1,29 @@
+from __future__ import division
+from itertools import combinations_with_replacement
+import numpy as np
+import math
+import sys
+
+def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    # Split the training data from test data in the ratio specified in
+    # test_size
+    split_i = len(y) - int(len(y) // (1 / test_size))
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
+
+
+def mean_squared_error(y_true, y_pred):
+
+    mse = np.mean(np.power(y_true - y_pred, 2))
+    return mse
+
+def shuffle_data(X, y, seed=None):
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
